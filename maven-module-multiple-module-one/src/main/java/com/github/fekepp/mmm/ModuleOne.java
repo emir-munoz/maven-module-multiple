937f602bb95de2347045b4df5f351ef0b4678943
Felix Leif Keppmann <felix.leif@keppmann.de>
2015-02-05 18:23:27 +0100

    Test code

---
+++ maven-module-multiple-module-one/src/main/java/com/github/fekepp/mmm/ModuleOne.java
@@ -0,0 +1,9 @@
+package com.github.fekepp.mmm;
+
+public class ModuleOne {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World from class "
+				+ ModuleOne.class.getSimpleName() + "!");
+	}
+}
---
+++ maven-module-multiple-module-two/src/main/java/com/github/fekepp/mmm/ModuleTwo.java
@@ -0,0 +1,9 @@
+package com.github.fekepp.mmm;
+
+public class ModuleTwo {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World from class "
+				+ ModuleTwo.class.getSimpleName() + "!");
+	}
+}
